925edbda38e6048056891c203a8edbcbec7e946f
euspod <euspod@hotmail.fr>
2023-06-05 02:12:41 +0400

    création d'une fonction buttonclicked pour detecter le bouton cliqué; base de switch pour chaque signal envoyé par les boutons

--- Backend/works.js
+++ Backend/works.js
@@ -33,65 +33,125 @@
 // const data = await fetch('http://localhost:5678/api/works');
 // const jsonData = await data.json();
 
-// 
-// async function fetchWorks () {
-//     const works = await fetch('http://localhost:5678/api/works');
-//     const jsonData = await works.json();
-//     return jsonData;
-// }
 
-// async function generateWorks() { 
-//     const works = await fetchWorks();
-//     let htmlContent = '';
-//     works.forEach(work => {
-//         let data =  `<figure>
-//                         <img src="${work.imageUrl}">
-//                         <figcaption>${work.title}</figcaption>
-//                         </figure>`;
+async function fetchWorks () {
+    const works = await fetch('http://localhost:5678/api/works');
+    const jsonData = await works.json();
+    return jsonData;
+}
+
+async function generateWorks() { 
+    const works = await fetchWorks();
+    let htmlContent = '';
+    works.forEach(work => {
+        let data =  `<figure>
+                        <img src="${work.imageUrl}">
+                        <figcaption>${work.title}</figcaption>
+                        </figure>`;
                         
-//     htmlContent += data;
-// })
-//     const gallery = document.querySelector('.gallery');
-//     gallery.innerHTML = htmlContent;
+    htmlContent += data;
+})
+    const gallery = document.querySelector('.gallery');
+    gallery.innerHTML = htmlContent;
 
-// }
+}
 // generateWorks();
-const buttons = document.querySelectorAll('.btn')
-const btnAll = document.querySelector('.all');
+
+
+
+// const buttons = document.querySelectorAll('.btn')
+const btnAll = document.getElementById('all');
 const btnObjects = document.querySelector('.objects');
 const btnApartments = document.querySelector('.apartments');
 const btnHotel = document.querySelector('.hotel');
 
 
- function setSelected() {
-    buttons.forEach(button => {
-        button.classList.remove('selected');
-    });
+//  function setSelected() {
+//     buttons.forEach(button => {
+//         button.classList.remove('selected');
+//     });
     
-}
-setSelected();
+// }
+// setSelected();
 
-fetch('http://localhost:5678/api/works')
-    .then(response => response.json())
-    .then(data => {
-             for(let i = 0; i < data.length; i++) {
+// fetch('http://localhost:5678/api/works')
+//     .then(response => response.json())
+//     .then(data => {
+//              for(let i = 0; i < data.length; i++) {
+//             //  console.log(data[i].categoryId)
+//     }
 
-             console.log(data[i].categoryId)
-    }
-
-})
-
-        
-        
-    
-
-  
-
-        
-    
-
+// })
 
  
+
+async function fetchCats () {
+    const cats = await fetch('http://localhost:5678/api/categories');
+    const jsonData = await cats.json();
+    const catAll = new Set;
+    catAll.add(jsonData[1]);
+    console.log(catAll);
+    
+}
+fetchCats();
+
+// async function generateCats() { 
+//         const cats = await fetchCats();
+//         let htmlContent = '';
+//         cats.forEach(cat => {
+//             let data =  `<figure>
+//                             <img src="${cat.imageUrl}">
+//                             <figcaption>${cat.title}</figcaption>
+//                             </figure>`;
+                            
+//         htmlContent += data;
+//         console.log(htmlContent);
+//     })  
+//  }    
+    
+// generateCats();
+  
+
+
+
+// function buttonClicked() {
+    
+    //     e.target.nodeName === 'BUTTON';
+    //     console.log(truc) ;
+    // }
+    
+const buttons = document.getElementById('group');
+
+
+
+const buttonClicked = (e) => {
+    e.target.nodeName === 'BUTTON';
+    // console.log(e.target.id);
+    return e.target.id;
+}
+buttons.addEventListener("click", buttonClicked);
+
+
+
+
+
+
+const buttonValue = buttonClicked;
+console.log(buttonValue);
+// switch(buttonValue) {
+//     case 'objects':
+//         /*gallery -> objectslist */
+//         break;
+//     case 'apartments':
+//     /*gallery -> apartmentslist */
+//     break;
+//     case 'hotel':
+//     /*gallery -> hotelslist */
+//     break;
+//     default:
+//     /*gallery -> allList */
+
+// } 
 
 
 /* categories : 
