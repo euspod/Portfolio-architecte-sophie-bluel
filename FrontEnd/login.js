606c1bd1106092e490e20ad765d441e7b9f0c18d
euspod <euspod@hotmail.fr>
2023-07-05 21:57:31 +0400

    Simplification du script login.js

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,42 +1,27 @@
 
-async function fetchPost(url, formData ) {
-	const rawData = Object.fromEntries(formData.entries()); /* convertit données de connexion de l'utilisateur dans un tableau paire clé/valeur,*/
-	const data = JSON.stringify(rawData);					/*puis en chaine de caractère, ajoute ces données aux optins de fetch, vérifie la réponse */
+async function submit(e) {    
+	e.preventDefault();
+	const formData = new FormData(submitBtn);
+    const url = 'http://localhost:5678/api/users/login';
+	const rawData = Object.fromEntries(formData.entries()); 
+	const data = JSON.stringify(rawData);					
 	const fetchOptions = {
 		method: "POST",
 		headers: {
 			"Content-Type": "application/json",
-			"Accept": "application/json",
-		},
-		body: data,
-	};
+			"Accept": "application/json",		
+		},										/* à partir des données du formulaire crée un objet formData, transforme c données en tableau clé/val */
+		body: data,								/* convertit l'objet en json, récupère la réponse de fetch,vérifie les erreurs de statut de la réponse et renvoie des messages à l'utilisateur */
+	};                              			/*  stocke le token reçu, redirige vers index.html  */
 	const response = await fetch(url, fetchOptions);
-	try {
-		
-		if(response.status === 404) throw `L'adresse email saisie est invalide.`;
-		if(response.status === 401) throw `Le mot de passe que vous avez indiqué n'est pas reconnu.`;
-		return response.json();
-
-	}catch(error) {
-
-		alert('Erreur: '+ error);
-		return;
-	};
-		
-}
-
-
-async function submit(e) {    
-	e.preventDefault();
-    const url = 'http://localhost:5678/api/users/login';
-
-	try {												/* récupère les données de fetch, stocke le token reçu, redirige vers index.html */
-        const formData = new FormData(submitBtn);
-        const responseData = await fetchPost( url, formData );
-		localStorage.setItem('token', responseData.token);
+	
+	try {												
+	if(response.status === 404) throw `L'adresse email saisie est invalide.`;
+	if(response.status === 401) throw `Le mot de passe que vous avez indiqué n'est pas reconnu.`;
+		localStorage.setItem('token', response.token);      
         window.location.href='index.html';
 	} catch (err) {
-		console.log('Erreur: '+ err);
+		alert('Erreur: '+ err);
         
 	}
 }
